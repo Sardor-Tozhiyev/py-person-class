230f9168b463024fd831f55321fa82c8e9f44413
Sardor <sardor.tozhiev1994@gmail.com>
2026-05-15 10:26:18 +0300

    added .get and list comprehension

--- app/main.py
+++ app/main.py
@@ -11,16 +11,18 @@ def create_person_list(people: list) -> list:
     person_list = []
 
     for person in people:
-        new_person = Person(person["name"], person["age"])
-        person_list.append(new_person)
+        person_list = [
+            Person(person["name"],
+                   person["age"]) for person in people
+        ]
 
     for person in people:
         current_person = Person.people[person["name"]]
 
-        if "wife" in person and person["wife"] is not None:
+        if person.get("wife") is not None:
             current_person.wife = Person.people[person["wife"]]
 
-        if "husband" in person and person["husband"] is not None:
+        if person.get("husband") is not None:
             current_person.husband = Person.people[person["husband"]]
 
     return person_list
